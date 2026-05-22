bf4fe5d76f58894f4556bfa250d784596195a9ca
iliketurbos <140767401+iliketurbos@users.noreply.github.com>
2026-05-22 13:33:48 -0700

    v2.0

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname.startsWith("/api")) {
-      return env.payday-proxy.fetch(request);
+      return env["payday-proxy"].fetch(request);
     }
 
     return env.ASSETS.fetch(request);
