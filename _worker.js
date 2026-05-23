7d2207052fa1edbc7d1cf471b54ec0e6564d7c87
iliketurbos <140767401+iliketurbos@users.noreply.github.com>
2026-05-22 20:16:23 -0700

    v2.0

--- _worker.js
+++ _worker.js
@@ -3,7 +3,14 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname.startsWith("/api")) {
-      return env["payday-proxy"].fetch(request);
+      return env["payday-proxy"].fetch(
+        "https://payday-proxy.paydayanalyst.workers.dev" + url.pathname,
+        {
+          method: "POST",
+          headers: request.headers,
+          body: await request.text(),
+        }
+      );
     }
 
     return env.ASSETS.fetch(request);
